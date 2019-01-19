1d185a6e1e936a986bc7a840fa4e2116e2d52c6a
Nando <mnandohidayat@gmail.com>
2019-01-20 04:41:53 +0700

    set variables, in the middle of editting index

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const nando = { name: "nando", age: 100, cool: true };
+  // res.send("Hey! It works!");
+  // res.json(nando);
+  res.send(req.query.name);
 });
 
 module.exports = router;
