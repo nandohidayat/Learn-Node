2c8cca52fc3f3c3d7349462e3e0b3130eb639d08
Nando <mnandohidayat@gmail.com>
2019-01-20 19:02:26 +0700

    add Store Schema

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,11 +1,10 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = "Nando";
-  next();
-};
-
 exports.homePage = (req, res) => {
   res.render("index", {
     title: "Home",
     name: req.name
   });
+};
+
+exports.addStore = (req, res) => {
+  res.send("It works!");
 };
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 
 // Do work here
-router.get("/", storeController.myMiddleware, storeController.homePage);
+router.get("/", storeController.homePage);
+router.get("/add", storeController.addStore);
 
 module.exports = router;
