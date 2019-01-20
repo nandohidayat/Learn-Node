663f76027dd4bb62bff7cb7ffc40437c65e0b01d
Nando <mnandohidayat@gmail.com>
2019-01-20 16:22:32 +0700

    add middleware

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,11 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = "Nando";
+  next();
+};
+
 exports.homePage = (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Home",
+    name: req.name
+  });
 };
