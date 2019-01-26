113e794bf694432681b1d043584884b1bb2c9004
Nando <mnandohidayat@gmail.com>
2019-01-26 12:56:29 +0700

    add flash message and redirect to store instead of homepage

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -13,7 +13,15 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect("/");
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  console.log(store);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = (req, res) => {
+  res.render("store", { title: "Store" });
 };
