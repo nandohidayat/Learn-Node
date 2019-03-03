e1c2dd46e15fcf367bba6c696c017208c728b03e
nandohidayat <mnandohidayat@gmail.com>
2019-03-03 23:55:19 +0700

    send email reset password

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const crypto = require("crypto");
 const promisify = require("es6-promisify");
+const mail = require("../handlers/mail");
 
 exports.login = passport.authenticate("local", {
   failureRedirect: "/login",
@@ -40,10 +41,13 @@ exports.forgot = async (req, res) => {
   const resetURL = `http://${req.headers.host}/account/reset/${
     user.resetPasswordToken
   }`;
-  req.flash(
-    "success",
-    `You have been emailed a password reset link. ${resetURL}`
-  );
+  await mail.send({
+    user,
+    filename: "password-reset",
+    subject: "Password Reset",
+    resetURL
+  });
+  req.flash("success", `You have been emailed a password reset link.`);
   res.redirect("/login");
 };
 
