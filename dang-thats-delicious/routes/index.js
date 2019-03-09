411322437244bcf49cf0111d04c1ce6b2562cb5e
nandohidayat <mnandohidayat@gmail.com>
2019-03-09 20:47:54 +0700

    add map view

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -50,6 +50,7 @@ router.post(
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get("/map", storeController.mapPage);
 
 // API
 router.get("/api/search", catchErrors(storeController.searchStores));
